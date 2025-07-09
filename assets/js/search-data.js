4dc9a1aabbef64eb6334c0ab02c6163e81f4d34d
xhu0101 <cecehubc@gmail.com>
2025-07-09 19:19:37 +0000

    Deploying to gh-pages from @ xhu0101/xhu0101.github.io@92298912c2f64c95117c58efdb715bfcd3e06fca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "books-the-godfather",
+  },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
